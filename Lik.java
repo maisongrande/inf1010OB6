0950d5f5d0384356801ce312fd6dc87efe2f706b
maisongrande <christopherstorheim@gmail.com>
2015-03-10 14:21:22 +0100

    lagt til Lik.java

---
+++ Lik.java
@@ -0,0 +1,14 @@
+/**
+ * Interface Lik skal sjekke om et objekt er 
+ * identiskt med et annet objekt det sjekkes opp
+ * imot. 
+ * @param navn, navnet knyttet til objektet
+ * @return <CODE>true</CODE> hvis objektet er identiskt
+ * @return <CODE>false</CODE> hvis uidentiskt.
+ */
+
+interface Lik {
+
+    boolean samme(String navn);
+
+}
