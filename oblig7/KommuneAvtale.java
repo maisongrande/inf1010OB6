ee216138e8dc9f12ce241adbee8b6b17c5adb30c
maisongrande <christopherstorheim@gmail.com>
2015-03-20 14:51:15 +0100

    tutut?

---
+++ oblig7/KommuneAvtale.java
@@ -0,0 +1,12 @@
+/**
+ * Interface KommuneAvtale.
+ * En kommuneavtale er en avtale mellom
+ * en person og kommunen.
+ * Naar en kommuneavtale opprettes gis personen
+ * ett avtalenummer.
+ */
+interface KommuneAvtale {
+
+    int avtaleNummer();
+
+} /* End KommuneAvtale */ 
