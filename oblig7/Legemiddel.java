6ad34d783135c00e2a6e72288608f79fd088d181
maisongrande <christopherstorheim@gmail.com>
2015-03-20 15:59:55 +0100

    blabb

--- oblig7/Legemiddel.java
+++ oblig7/Legemiddel.java
@@ -1,12 +1,24 @@
 public abstract class Legemiddel  {
-  private String navn;
-  private int nummer;
-  private int pris;
+    private String navn;
+    private int nummer;
+    private int pris;
     private static int last_nummer=0;
     
     public Legemiddel(String navn, int pris){
         this.navn = navn;
         this.pris = pris;
         nummer = ++last_nummer;
+    }
+
+    public String getNavn() {
+	return navn;
+    }
+
+    public int getNummer() {
+	return nummer;
+    }
+
+    public int getPris() {
+	return pris;
     }
 }
